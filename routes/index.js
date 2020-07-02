788cc34cc15bcb269ab67d536f28d173fe8cdd84
Axel Kerbec <axelkerbec@Axels-MacBook-Pro.local>
2020-07-02 18:23:56 +0100

    Added Database connection check

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../Algorithm/queries');
+var q = require('q');
+var database = require('../Algorithm/db_connection')
+var getdata_controller = require('../Algorithm/getData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = getdata_controller.makeQuery(database, q, "SELECT * FROM Symptom", "id", "definition", "symptom_name")
+   .then(function(rows)  {
+     var symptoms = list;
+     res.render('index', { title: 'Express', symptoms: symptoms });
+     // return symptoms;
+   });
+   // return query;
 });
 
 module.exports = router;

21d483397be06273b4eb2ca0f9e66f86ae14a6e6
Daphne Demekas <zcahdde@ucl.ac.uk>
2020-07-03 10:27:33 +0200

    renamed algorithm folder

--- Algorithm/testing.js
+++
@@ -1,5 +0,0 @@
-var queries = require('./Algorithm/queries');
-var q = require('q');
-var database = require('./Algorithm/db_connection')
-var getdata_controller = require('./Algorithm/getData');
-var bayesionmodel = require('./Algorithm/bayesionmodel');
